bee0b06fb5065f5b5328bbbe16bbc673e81d0663
Ahmad Mozaffar <aksoftware98@hotmail.com>
2021-10-06 01:27:47 +0300

    Implement an effiecient function to check if a number is prime or no

---
+++ AlgorithmsAndDS/Math01/MathFunctions.cs
@@ -0,0 +1,33 @@
+﻿namespace Math01
+{
+    public class MathFunctions
+    {
+
+        #region Prime Number
+        /// <summary>
+        /// This function indicates if a specific number is a prime or not
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsPrime(int number)
+        {
+            if (number == 1)
+                return false;
+
+            if (number == 2 || number == 3)
+                return true;
+
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        #endregion 
+    }
+}
