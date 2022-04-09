a207ba3d6844472732f9d87e6204777643b37ae7
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-04-09 16:18:45 +0400

    Working with recursion part 1

---
+++ AlgorithmsAndDS/Recurrersion02/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Penlndrom 
+//while (true)
+//{
+//    Console.WriteLine("Palendrome Solutions (Enter a new word):");
+//    var word = Console.ReadLine();
+//    Console.WriteLine(Recurrersion02.RecurresionSolutions.IsPalendrome(word));
+//    Console.WriteLine("Press any key to try again");
+//    Console.ReadKey();
+//}
+
+// Check out first 
+Console.WriteLine("Set a number");
+int? number = Convert.ToInt32(Console.ReadLine());
+Recurrersion02.RecurresionSolutions.Fun1(number.Value); 
---
+++ AlgorithmsAndDS/Recurrersion02/RecurresionSolutions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurrersion02
+{
+    public static class RecurresionSolutions
+    {
+        #region Palindrome 
+        public static bool IsPalendrome(string text)
+        {
+            return IsPalendrome(text, 0, text.Length - 1);
+        }
+
+        private static bool IsPalendrome(string text, int start, int end)
+        {
+            if (start >= end)
+                return true;
+
+            return text[start] == text[end] && IsPalendrome(text, start + 1, end - 1);
+
+        }
+        #endregion
+
+        #region Output check
+        public static void Fun1(int number)
+        {
+            if (number == 0)
+                return;
+
+            Console.WriteLine(number);
+            Fun1(number - 1);
+            Console.WriteLine(number);
+        }
+
+        #endregion 
+    }
+}
