bd2b1d19dde961df587813281f19aecee88580f0
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-05-02 15:37:20 +0400

    Check Array is sorted

--- AlgorithmsAndDS/Arrays03/Program.cs
+++ AlgorithmsAndDS/Arrays03/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 var array = new[] { 10, 12, 42, 41, 11, 1, 42, 4 };
+var array2 = new[] { 10, 12, 42, 41, 11, 1, 42, 4 };
+Array.Sort(array2);
 Max(array);
 SecondMax(array);
+IsSorted(array2);
 
 Console.ReadKey();
 
@@ -43,5 +46,20 @@ static void SecondMax(int[] array)
     }
 
     Console.WriteLine($"Second max {previous}");
+}
+
+static void IsSorted(int[] array)
+{
+    bool isSorted = true;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i > 0 && array[i] < array[i - 1])
+        {
+            isSorted = false;
+            break; 
+        }
+    }
+
+    Console.WriteLine($"Is Sorted {isSorted}");
 }
 #endregion 
